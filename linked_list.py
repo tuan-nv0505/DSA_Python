438457bf2e65827c12bd79ce3434da7304361a96
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-09-16 20:58:15 +0700

    update

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,3 @@
-
-
-
 class Node:
     def __init__(self, key):
         self.key = key
@@ -9,8 +6,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        
-    # Them vao cuoi danh sach
+
     def insert_last(self, key):
         if self.head is None:
             self.head = Node(key)
@@ -19,8 +15,7 @@ class LinkedList:
             while tmp.next is not None:
                 tmp = tmp.next
             tmp.next = Node(key)
-            
-    # Them vao dau danh sach
+
     def insert_first(self, key):
         if self.head is None:
             self.head = Node(key)
@@ -28,8 +23,7 @@ class LinkedList:
             new_node_first = Node(key)
             new_node_first.next = self.head
             self.head = new_node_first
-    
-    # Xoa phan tu o cuoi danh sach
+
     def remove_last(self):
         if self.head is None:
             return
@@ -40,7 +34,27 @@ class LinkedList:
             while tmp.next.next is not None:
                 tmp = tmp.next
             tmp.next = None
-            
+
+    def remove_first(self):
+        if self.head is None:
+            return
+        elif self.head.next is None:
+            self.head = None
+        else:
+            tmp = self.head
+            while tmp.next.next is not None:
+                tmp = tmp.next
+            tmp.next = None
+
+    def find_node_first(self, key):
+        pass
+
+    def find_node_last(self, key):
+        pass
+
+    def sort(self):
+        pass
+
     def show(self):
         tmp = self.head
         while tmp is not None:
@@ -61,7 +75,10 @@ if __name__ == "__main__":
     
     for i in range(5):
         ll.remove_last()
-    
+
+    for i in range(5):
+        ll.remove_first()
+
     ll.show()
     
     
